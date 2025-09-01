e3e3f4855163036e06a463ef5e83f32a3ca8d421
Stanley Masinde <stanmasinde@gmail.com>
2025-09-02 01:44:38 +0300

    move the cli log to the cli mod

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,5 +1,11 @@
 use clap::Parser;
 
+use crate::{
+    api::client::{ApiClient, HttpClient},
+    server::{self, routes::api::CreateTweet},
+    twitter::tweet,
+};
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
@@ -14,4 +20,31 @@ pub struct Args {
     /// Create a new tweet
     #[arg[short, long]]
     pub tweet: Option<String>,
+}
+
+pub async fn run() {
+    let args = std::env::args().collect::<Vec<_>>();
+
+    if args.len() == 1 {
+        Args::parse_from(["", "--help"]);
+    }
+    let args = Args::parse();
+
+    if args.serve {
+        let port = args.port;
+        server::run(port).await
+    }
+
+    if let Some(tweet_text) = args.tweet {
+        let client = ApiClient::new();
+        let payload = CreateTweet { text: tweet_text };
+        let api_res = tweet::create(client, payload).await;
+
+        match api_res {
+            Ok(ok) => {
+                println!("{}", ok.content)
+            }
+            Err(err) => println!("Error:{}", err),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use clap::Parser;
-use log::{error, info};
-
-use crate::{
-    api::client::{ApiClient, HttpClient},
-    cli::Args,
-    server::routes::api::CreateTweet,
-    twitter::tweet,
-};
-
 pub mod api;
 pub mod cli;
 pub mod config;
@@ -16,23 +6,5 @@ pub(crate) mod twitter;
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
-
-    if args.serve {
-        let port = args.port;
-        server::run(port).await
-    }
-
-    if let Some(tweet_text) = args.tweet {
-        let client = ApiClient::new();
-        let payload = CreateTweet { text: tweet_text };
-        let api_res = tweet::create(client, payload).await;
-
-        match api_res {
-            Ok(ok) => {
-                println!("{}", ok.content)
-            }
-            Err(err) => error!("Error:{}", err),
-        }
-    }
+    cli::run().await;
 }
