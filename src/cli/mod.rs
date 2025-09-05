1febfe0a5d9a8f2675e670f54ffc35b67bfd106e
Stanley Masinde <stanmasinde@gmail.com>
2025-09-06 00:12:59 +0300

    feat: trim text piped in and informative error message.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -44,9 +44,11 @@ pub async fn run() {
                 Some(tweet) => tweet,
                 None => {
                     let mut buf = String::new();
-                    io::stdin().read_to_string(&mut buf).unwrap(); // I don't expect this to fail.
+                    io::stdin()
+                        .read_to_string(&mut buf)
+                        .expect("Failed to read tweet!");
 
-                    buf
+                    buf.trim().to_string()
                 }
             };
 
